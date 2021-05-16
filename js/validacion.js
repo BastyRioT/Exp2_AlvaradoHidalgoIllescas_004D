046a7ab110cce70cfb4ead6d714f2cda9900270c
BastyRioT <ba.alvaradog@aduocuc.cl>
2021-05-16 18:48:02 -0400

    Se mejoró la validación de datos del sitio registro, ahora es por medio de JQuery

--- js/validacion.js
+++ js/validacion.js
@@ -1,58 +1,41 @@
-function validacion() {
+$(function validarregistro() 
+{
+  $("#registro").validate({
+       rules: {
+              email: {
+                  required: true,
+                  email: true
+              },
+              usuario: "required",
+              password: "required",
+              password2: {
+                  required: true,
+                  equalTo: "#password"
+              },
+              fecha: "required" 
+          }, //rules
+      messages: {
+          email: {
+              required: 'Ingresa tu correo electrónico',
+              email: 'Formato de correo no válido'
+          },
+          usuario: {
+              required: 'Ingresa tu nombre de usuario'
+          },
+          password: {
+              required: 'Ingresa una contraseña',
+              minlength: 'Caracteres insuficientes'
+          },
+          password2: {
+            required: 'Reingresa la contraseña',
+            equalTo: 'Las contraseñas ingresadas no coinciden',
+            minlength: 'Caracteres insuficientes'
+          },
+          fecha:{
+              required: 'Ingresa tú fecha de nacimiento',
+              min: 'Fecha de nacimiento no válida'
+          }
+      }//messages
+  }); //$('registro').validate
+}); //function
 
-    mail = document.getElementById('correo').value;
-    user = document.getElementById('usuario').value;
-    p1 = document.getElementById('clave').value;
-    p2 = document.getElementById('clave2').value;
-    nac = document.getElementById('fechanac').value;
-
-    /*Correo*/
-    if (!(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(mail))) {
-        alert("Debe ingresar un correo electronico valido");
-        document.getElementById('correo').focus();
-        return false;
-    }
-
-    /*Usuario*/
-    if(user.length==0) {
-        alert('Debe ingresar un nombre de usuario');
-        document.getElementById('usuario').focus();
-        return false;
-    }
-
-    /*Contraseña*/
-    var espacios = false;
-    var cont = 0;
-
-    while (!espacios && (cont < p1.length)) {
-        if (p1.charAt(cont) == " ")
-            espacios = true;
-        cont++;
-    }
-        
-    if (espacios) {
-        alert ("La contraseña no puede contener espacios en blanco");
-        document.getElementById('clave').focus();
-        return false;
-    }
-    
-    if (p1.length == 0) {
-        alert("Los campos de contraseña no pueden quedar vacios");
-        document.getElementById('clave').focus();
-        return false;
-    }
-
-    if (p2.length == 0) {
-        alert("Debe verificar la contraseña");
-        document.getElementById('clave2').focus();
-        return false;
-    }
-
-      if (p1 != p2) {
-        alert("Las contraseñas deben de coincidir");
-        document.getElementById('clave2').focus();
-        return false;
-    }
-    
-    return true;
-}
